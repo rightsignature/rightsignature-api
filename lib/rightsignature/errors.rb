a23b3f19985f9816baefe3e8ece177c4d86aaef4
Alex Chee <alexchee11@gmail.com>
2012-10-16 14:57:08 -0700

    made TokenResponseError and OAuthResponseError inherit from ResponseError

--- lib/rightsignature/errors.rb
+++ lib/rightsignature/errors.rb
@@ -8,10 +8,12 @@ module RightSignature
       super((message || @response.message))
     end
 
+    # returns HTTP Code from response
     def code
       @response.code
     end
   
+    # Suggestions on how to resolve a certain error 
     def common_solutions
       if @response.code.to_i == 406
         "Check the Content-Type and makes sure it's the correct type (usually application/json or application/xml), ensure url has .xml or .json at the end, check 'Accept' header to allow xml or json ('*/*' for anything)"
@@ -23,6 +25,8 @@ module RightSignature
     end
   end
 
-  class TokenResponseError < Exception; end
-  class OAuthResponseError < Exception; end
+  class TokenResponseError < ResponseError # :nodoc:
+  end
+  class OAuthResponseError < ResponseError # :nodoc:
+  end
 end
